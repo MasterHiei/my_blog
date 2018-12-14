beaa5c616df7b4cb8b806ce31c5a6fb388086fc9
Ruhr <ruhrg.lcx@gmail.com>
2018-12-14 17:39:58 +0900

    Add parts to navigation bar

--- src/components/Navbar/TheNavbar.vue
+++ src/components/Navbar/TheNavbar.vue
@@ -1,25 +1,40 @@
 <template>
-  <nav id="navbar" class="navbar is-fixed-top is-light" role="navigation" aria-label="main navigation">
-    <div class="navbar-brand">
-      <a class="navbar-item" href="#">Brand</a>
+  <nav id="navbar" class="navbar is-fixed-top is-dark" role="navigation" aria-label="main navigation">
+    <div id="brand" class="navbar-brand">
+      <a id="logo" href="/home" class="navbar-item">
+        <strong>My Blog</strong>
+      </a>
+      <div class="navbar-item">
+        <div class="navbar-item is-hidden-desktop">
+          <div class="field">
+            <div class="control">
+              <input id="searchInput" class="input" type="text" placeholder="Search...">
+            </div>
+          </div>
+        </div>
+      </div>
 
-      <a role="button" class="navbar-burger burger is-hidden-desktop" aria-label="menu" aria-expanded="false" data-target="menuList">
+      <a role="button" :class="[burgerClass, isActive ? activeClass : '']"
+      aria-label="menu" aria-expanded="false" data-target="menuList"
+      @click="onClick">
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
       </a>
     </div>
 
-    <div id="menuList" class="navbar-menu">
+    <div id="menuList" :class="[menuClass, isActive ? activeClass : '']">
       <div class="navbar-start">
         <a class="navbar-item">Home</a>
         <a class="navbar-item">Articles</a>
       </div>
       <div class="navbar-end">
-        <div class="navbar-item">
-          <div class="buttons">
-          <a class="button is-success">Create</a>
-        </div>
+        <div class="navbar-item is-hidden-touch">
+          <div class="field">
+            <div class="control">
+              <input id="searchInput" class="input" type="text" placeholder="Search...">
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -28,8 +43,29 @@
 
 <script>
 export default {
+  data () {
+    return {
+      isActive: false,
+      burgerClass: 'navbar-burger burger is-hidden-desktop',
+      menuClass: 'navbar-menu',
+      activeClass: 'is-active'
+    }
+  },
+  methods: {
+    onClick () {
+      this.isActive = !this.isActive
+    }
+  }
 }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  #brand
+    margin-left 0
+
+  #menuList
+    margin-right 0
+
+  #searchInput
+    height 32px
 </style>
