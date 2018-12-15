de08ba303629dd065f4eb34136b33f7b744b4234
MasterHiei <ruhrg.lcx@gmail.com>
2018-12-15 19:45:22 +0900

    fix navbar style, add vue-i18n dependence.

--- src/components/Navbar/TheNavbar.vue
+++ src/components/Navbar/TheNavbar.vue
@@ -1,38 +1,37 @@
 <template>
   <nav id="navbar" class="navbar is-fixed-top is-dark" role="navigation" aria-label="main navigation">
     <div id="brand" class="navbar-brand">
-      <a id="logo" href="/home" class="navbar-item">
+      <a class="navbar-item" role="button" @click="transTo('/home')">
         <strong>My Blog</strong>
       </a>
       <div class="navbar-item">
         <div class="navbar-item is-hidden-desktop">
           <div class="field">
             <div class="control">
-              <input id="searchInput" class="input" type="text" placeholder="Search...">
+              <input class="input" type="text" placeholder="Search...">
             </div>
           </div>
         </div>
       </div>
 
-      <a role="button" :class="[burgerClass, isActive ? activeClass : '']"
-      aria-label="menu" aria-expanded="false" data-target="menuList"
-      @click="onClick">
+      <a id="navbarBurger" :class="['navbar-burger burger is-hidden-desktop', { 'is-active': isActive }]" role="button"
+      aria-label="menu" aria-expanded="false" @click="onClick">
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
       </a>
     </div>
 
-    <div id="menuList" :class="[menuClass, isActive ? activeClass : '']">
+    <div :class="['navbar-menu', { 'is-active': isActive }]">
       <div class="navbar-start">
-        <a class="navbar-item">Home</a>
-        <a class="navbar-item">Articles</a>
+        <a role="button" class="navbar-item" @click="transTo('/home')">Home</a>
+        <a role="button" class="navbar-item" @click="transTo('/articles')">Articles</a>
       </div>
       <div class="navbar-end">
         <div class="navbar-item is-hidden-touch">
           <div class="field">
             <div class="control">
-              <input id="searchInput" class="input" type="text" placeholder="Search...">
+              <input class="input" type="text" placeholder="Search...">
             </div>
           </div>
         </div>
@@ -45,27 +44,26 @@
 export default {
   data () {
     return {
-      isActive: false,
-      burgerClass: 'navbar-burger burger is-hidden-desktop',
-      menuClass: 'navbar-menu',
-      activeClass: 'is-active'
+      isActive: false
     }
   },
   methods: {
     onClick () {
       this.isActive = !this.isActive
+    },
+    transTo (path) {
+      this.onClick()
+      this.$router.replace(path)
     }
   }
 }
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  #brand
-    margin-left 0
+  @media (min-width: 1024px)
+    #navbar
+      padding 8px 12px 8px  12px
 
-  #menuList
-    margin-right 0
-
-  #searchInput
-    height 32px
+  #navbarBurger
+    height: auto
 </style>
--- src/main.js
+++ src/main.js
@@ -2,10 +2,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import VueI18n from 'vue-i18n'
 import Buefy from 'buefy'
 import 'buefy/dist/buefy.css'
 
+Vue.use(VueI18n)
 Vue.use(Buefy)
+
+// vue-i18n Configuration
+Vue.config.lang = 'ja'
 
 /* eslint-disable no-new */
 new Vue({
